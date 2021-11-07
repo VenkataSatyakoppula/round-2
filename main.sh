b5a541772f7d55370527752660b46b57213021b7
VenkataSatyakoppula <satya5202@gmail.com>
2021-11-07 21:36:45 +0530

    final commit i think

--- main.sh
+++ main.sh
@@ -7,20 +7,24 @@ echo -e "\n\tYour PC-Health Basically :) \033[0m\n"
 
 pcname=$(cat /proc/sys/kernel/hostname)
 Username=$USER
-file="/sys/class/power_supply/BAT0/capacity"
+file="/sys/class/power_supply/BAT0"
 kernel=$(cat /proc/version)
-memory=$(cat  /proc/meminfo | head -3 | sed 's/^/        /')
-
-
+memory=$(cat  /proc/meminfo | head -10 | sed 's/^/        /')
+cpuinfo=$(cat /proc/cpuinfo | head -16 | sed 's/^/        /')
 if [ -e "$file" ]
 then
-    batterylevel=$(cat ${file})
-    batteryhealth=$(cat ${file}_level)
+    batterylevel=$(cat ${file}/capacity)
+    batteryhealth=$(cat ${file}/capacity_level)
+    design_capacity=$(cat ${file}/charge_full)
+    last_full_capacity=$(cat ${file}/charge_full_design)
+
 else
     batterylevel=$(cat /sys/class/power_supply/BAT1/capacity)
     batteryhealth=$(cat /sys/class/power_supply/BAT1/capacity_level)
-fi
+    design_capacity=$(cat /sys/class/power_supply/BAT1/charge_full)
+    last_full_capacity=$(cat /sys/class/power_supply/BAT1/charge_full_design)
 
+fi
 echo -e "${PURPLE}PC-NAME :\033[0m ${pcname} \n"
 echo -e "${PURPLE}USERNAME :\033[0m ${Username} \n "
 echo -e "${PURPLE}MEMORY:\033[0m"
@@ -28,5 +32,7 @@ echo -e "${memory}"
 echo -e "\n${PURPLE}KERNEL: \033[0m ${kernel}\n"
 echo -e "${PURPLE}BATTERY LEVEL :\033[0m ${batterylevel}%\n"
 echo -e "${PURPLE}BATTERY HEALTH:\033[0m ${batteryhealth}\n"
-
+echo -e "${PURPLE}DESIGN CAPACITY:\033[0m ${design_capacity}A\n"
+echo -e "${PURPLE}DESIGN CAPACITY:\033[0m ${last_full_capacity}A\n"
+echo -e "\n${PURPLE}CPU-INFO: \033[0m \n${cpuinfo}\n"
 echo -e " ${BLUE} -Made by team avengers \033[0m\n"
